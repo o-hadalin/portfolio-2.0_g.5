fce7bf291640ed4099d8b9bc13ad9b929fe13c37
O. HADALIN <dyblikat777@gmail.com>
2024-11-20 08:48:21 +0200

    Start structure and settings

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Swiper from "swiper";
+import './swiper/swiper-bundle.min.css';
+import Accordion from "simple-accordion";
+import "./js/my-projects.js";
+import "./js/faq.js";
+import "./js/reviews.js";
+import "./js/work-together.js";
+import "./js/mobile-menu.js";

7f06a9c70a55fe9b37c63d0e7163859ad3dd8167
O. HADALIN <dyblikat777@gmail.com>
2024-11-21 21:04:45 +0200

    Remove import library from main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-import Swiper from 'swiper';
-import Accordion from 'simple-accordion';
-import Swal from 'sweetalert2';
 import './js/my-projects.js';
 import './js/faq.js';
 import './js/reviews.js';

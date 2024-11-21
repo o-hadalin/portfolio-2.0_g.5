887315037de37e434e433d4622293cc6e821557c
Inna Khalas <innamakarec349@gmail.com>
2024-11-22 02:20:26 +0400

    header,mob-menu

---
+++ src/js/dark-theme.js
@@ -0,0 +1,34 @@
+const themeToggle = document.querySelector("#toggle");
+const themeToggleMob = document.querySelector("#toggle-mob");
+
+const currentTheme = localStorage.getItem("theme" || "light");
+
+if (currentTheme === "dark") {
+    document.body.classList.add("dark-theme");
+    themeToggle.checked = true;
+    // themeToggleMob.checked = true;
+} else {
+    document.body.classList.remove("dark-theme");
+    themeToggle.checked = false;
+    themeToggleMob.checked = false;
+}
+
+themeToggle.addEventListener("change", changeThemeToggle);
+themeToggleMob.addEventListener("change", changeThemeToggle);
+
+function changeThemeToggle() {
+    const isDark = themeToggle.checked || themeToggleMob.checked;
+
+    if (isDark) {
+        document.body.classList.add("dark-theme");
+        localStorage.setItem("theme", "dark");
+    } else {
+        document.body.classList.remove("dark-theme");
+        localStorage.setItem("theme", "light");
+    }
+
+    themeToggle.checked = isDark;
+    themeToggleMob.checked = isDark;
+}
+
+

19d65710a8f701de3211291c3a05be8fe4bcafbc
Pasha Nikolauchyk <pasha.nikolauchyk044@ukr.net>
2024-11-22 13:46:56 +0200

    fix faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,17 +1,7 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
 document.addEventListener('DOMContentLoaded', () => {
   new Accordion('.accordion-container', {
     duration: 300,
     openOnInit: [0],
     collapse: true,
   });
-});
-
-document.querySelectorAll('.ac').forEach(el => {
-  el.style.marginTop = '0';
-  el.style.borderBottom = '1px solid #e4e5e6';
-  el.style.backgroundColor = 'transparent';
-  //   el.style.border = 'none';
 });

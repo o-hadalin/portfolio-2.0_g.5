15204980cea2f807f9f218de5b0bfa6f7a1c4b27
O. HADALIN <dyblikat777@gmail.com>
2024-11-26 06:47:20 +0200

    Fav files

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -6,9 +6,9 @@ const currentTheme = localStorage.getItem("theme") || "light";
 
 function updateFavicon(isDark) {
     if (isDark) {
-        favicon.href = "/favicon-dark.svg";
+        favicon.href = "./img/favicon-dark.svg";
     } else {
-        favicon.href = "/favicon-light.svg";
+        favicon.href = "./img/favicon-light.svg";
     }
 }
 

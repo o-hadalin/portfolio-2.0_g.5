0d427c3ff6af2a94be0720322f5dbf7de73fdaa7
O. HADALIN <dyblikat777@gmail.com>
2024-11-26 06:07:51 +0200

    Favicon b&w + modify js for theme

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,16 +1,27 @@
 const themeToggleHeader = document.querySelector(".header .header-toggle");
 const themeToggleMobile = document.querySelector(".mob-nav .mobile-toggle");
+const favicon = document.querySelector("link[rel='icon']");
 
 const currentTheme = localStorage.getItem("theme") || "light";
+
+function updateFavicon(isDark) {
+    if (isDark) {
+        favicon.href = "./img/favicon-dark.svg";
+    } else {
+        favicon.href = "./img/favicon-light.svg";
+    }
+}
 
 if (currentTheme === "dark") {
     document.body.classList.add("dark-theme");
     themeToggleHeader.checked = true;
     themeToggleMobile.checked = true;
+    updateFavicon(true);
 } else {
     document.body.classList.remove("dark-theme");
     themeToggleHeader.checked = false;
     themeToggleMobile.checked = false;
+    updateFavicon(false);
 }
 
 themeToggleHeader.addEventListener("change", () => handleThemeToggle(themeToggleHeader));
@@ -29,6 +40,6 @@ function handleThemeToggle(toggle) {
 
     themeToggleHeader.checked = isDark;
     themeToggleMobile.checked = isDark;
+
+    updateFavicon(isDark);
 }
-
-

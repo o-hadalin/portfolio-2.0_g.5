ac6c1c3df762022d726631adb37914492c9580c9
O. HADALIN <dyblikat777@gmail.com>
2024-11-27 12:19:33 +0200

    Path fav - 2

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,40 +1,68 @@
-const themeToggleHeader = document.querySelector(".header .header-toggle");
-const themeToggleMobile = document.querySelector(".mob-nav .mobile-toggle");
-const favicon = document.querySelector("link[rel='icon']");
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggleHeader = document.querySelector(".header .header-toggle");
+  const themeToggleMobile = document.querySelector(".mob-nav .mobile-toggle");
+  const favicon = document.querySelector("link[rel='icon']");
 
-const lightFavicon = "/img/favicon-light.svg";
-const darkFavicon = "/img/favicon-dark.svg";
+  const lightFavicon = "/img/favicon-light.svg";
+  const darkFavicon = "/img/favicon-dark.svg";
 
-const currentTheme = localStorage.getItem("theme") || "light";
+  const currentTheme = localStorage.getItem("theme") || "light";
 
-if (currentTheme === "dark") {
-    document.body.classList.add("dark-theme");
-    favicon.setAttribute("href", darkFavicon);
-    themeToggleHeader.checked = true;
-    themeToggleMobile.checked = true;
-} else {
-    document.body.classList.remove("dark-theme");
-    favicon.setAttribute("href", lightFavicon);
-    themeToggleHeader.checked = false;
-    themeToggleMobile.checked = false;
-}
+  if (currentTheme === "dark") {
+      document.body.classList.add("dark-theme");
+      favicon.setAttribute("href", darkFavicon);
+      themeToggleHeader.checked = true;
+      themeToggleMobile.checked = true;
+  } else {
+      document.body.classList.remove("dark-theme");
+      favicon.setAttribute("href", lightFavicon);
+      themeToggleHeader.checked = false;
+      themeToggleMobile.checked = false;
+  }
 
-themeToggleHeader.addEventListener("change", () => handleThemeToggle(themeToggleHeader));
-themeToggleMobile.addEventListener("change", () => handleThemeToggle(themeToggleMobile));
+  themeToggleHeader.addEventListener("change", () => handleThemeToggle(themeToggleHeader));
+  themeToggleMobile.addEventListener("change", () => handleThemeToggle(themeToggleMobile));
 
-function handleThemeToggle(toggle) {
-    const isDark = toggle.checked;
+  function handleThemeToggle(toggle) {
+      const isDark = toggle.checked;
 
-    if (isDark) {
-        document.body.classList.add("dark-theme");
-        favicon.setAttribute("href", darkFavicon);
-        localStorage.setItem("theme", "dark");
+      if (isDark) {
+          document.body.classList.add("dark-theme");
+          favicon.setAttribute("href", darkFavicon);
+          localStorage.setItem("theme", "dark");
+      } else {
+          document.body.classList.remove("dark-theme");
+          favicon.setAttribute("href", lightFavicon);
+          localStorage.setItem("theme", "light");
+      }
+
+      themeToggleHeader.checked = isDark;
+      themeToggleMobile.checked = isDark;
+  }
+});
+
+
+
+fetch(lightFavicon)
+  .then(response => {
+    if (response.ok) {
+      console.log("Light favicon is available.");
     } else {
-        document.body.classList.remove("dark-theme");
-        favicon.setAttribute("href", lightFavicon);
-        localStorage.setItem("theme", "light");
+      console.error("Light favicon is not available.");
     }
+  })
+  .catch(error => {
+    console.error("Error fetching the light favicon:", error);
+  });
 
-    themeToggleHeader.checked = isDark;
-    themeToggleMobile.checked = isDark;
-}
+fetch(darkFavicon)
+  .then(response => {
+    if (response.ok) {
+      console.log("Dark favicon is available.");
+    } else {
+      console.error("Dark favicon is not available.");
+    }
+  })
+  .catch(error => {
+    console.error("Error fetching the dark favicon:", error);
+  });

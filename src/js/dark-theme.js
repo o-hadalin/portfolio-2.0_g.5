75d079d243bee5dd3549bbdd798c49fc12ed4aa4
O. HADALIN <dyblikat777@gmail.com>
2024-11-27 11:10:17 +0200

    Fav + modify JS theme

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,14 +1,20 @@
 const themeToggleHeader = document.querySelector(".header .header-toggle");
 const themeToggleMobile = document.querySelector(".mob-nav .mobile-toggle");
+const favicon = document.querySelector("link[rel='icon']");
+
+const lightFavicon = "./img/favicon-light.svg";
+const darkFavicon = "./img/favicon-dark.svg";
 
 const currentTheme = localStorage.getItem("theme") || "light";
 
 if (currentTheme === "dark") {
     document.body.classList.add("dark-theme");
+    favicon.setAttribute("href", darkFavicon);
     themeToggleHeader.checked = true;
     themeToggleMobile.checked = true;
 } else {
     document.body.classList.remove("dark-theme");
+    favicon.setAttribute("href", lightFavicon);
     themeToggleHeader.checked = false;
     themeToggleMobile.checked = false;
 }
@@ -21,9 +27,11 @@ function handleThemeToggle(toggle) {
 
     if (isDark) {
         document.body.classList.add("dark-theme");
+        favicon.setAttribute("href", darkFavicon);
         localStorage.setItem("theme", "dark");
     } else {
         document.body.classList.remove("dark-theme");
+        favicon.setAttribute("href", lightFavicon);
         localStorage.setItem("theme", "light");
     }
 
